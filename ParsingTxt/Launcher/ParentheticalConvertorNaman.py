8fba15b603169a2159e45fa4635ccafee6cb41f1
Harnoor <hsingh9@student.gsu.edu>
2019-04-20 18:42:56 -0400

    refactoring

--- ParsingTxt/Launcher/ParentheticalConvertorNaman.py
+++ ParsingTxt/Launcher/ParentheticalConvertorNaman.py
@@ -1,7 +1,4 @@
-string = "(dog,((cat,cow)carrot,bird));"
 stack = []
-val1 = ""
-val2 = ""
 count = 1
 Graph = {}
 
@@ -12,35 +9,43 @@ def createTree(v1, v2):
 
     parent = "internalNode" + str(count) + ""
     count += 1
-    Graph[parent]= (v1, v2)
+    Graph[parent] = (v1, v2)
     # print(Graph)
     return parent[::-1]
 
 
-for i in string:
-    if i == ")":
-        print(i.index())
-        # nextChr = string[i+1]
-        # if nextChr != "" || nextChr != ",":
-        j = stack.pop()
-        while j != ",":
-            val2 += str(j)
+def returnDictionary(newick):
+    val1 = ""
+    val2 = ""
+    for i in newick:
+        if i == ")":
+            # nextChr = string[i+1]
+            # if nextChr != "" || nextChr != ",":
             j = stack.pop()
-        val2 = val2[::-1]
-        j = stack.pop()     # skip over ","
-        while j != "(":
-            val1 += j
-            j = stack.pop()
-        val1 = val1[::-1]
-        # stack.pop() # skip over the ")"
-        node = createTree(val1, val2)
-        val1, val2 = "", ""
-        # if node[0] != str(count-1):
-        #     print(node[0] + "   " + str(count-1))
-        #     node.replace("internalNode"+ str(count), "")
-        #     print(node)
-        stack.append(node)
-    else:
-        stack.append(i)
+            while j != ",":
+                val2 += str(j)
+                j = stack.pop()
+            val2 = val2[::-1]
+            j = stack.pop()  # skip over ","
+            while j != "(":
+                val1 += j
+                j = stack.pop()
+            val1 = val1[::-1]
+            # stack.pop() # skip over the ")"
+            node = createTree(val1, val2)
+            val1, val2 = "", ""
+            # if node[0] != str(count-1):
+            #     print(node[0] + "   " + str(count-1))
+            #     node.replace("internalNode"+ str(count), "")
+            #     print(node)
+            stack.append(node)
+        else:
+            stack.append(i)
+    return Graph
 
-print(Graph)
+
+if __name__ == '__main__':
+    newick = "(dog,((cat,cow)carrot,bird));"
+    grph = returnDictionary(newick)
+
+    print(grph)
