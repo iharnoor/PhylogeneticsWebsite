d5d66974b645719ba9e4cc484753251435c8fc25
Harnoor <hsingh9@student.gsu.edu>
2019-11-05 13:41:49 -0500

    size is variable. graph fits svg very well

--- NewWebsite/sketch.js
+++ NewWebsite/sketch.js
@@ -680,7 +680,6 @@ function pushChangeRootToServer(flag, tripRoot) {
     var data = JSON.stringify({
         "text": flag
     });
-    ;
 
     var request = new XMLHttpRequest();
     request.open("POST", "http://127.0.0.1:5000/changeRoot/" + tripRoot);
